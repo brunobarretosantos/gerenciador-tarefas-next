ff525a8cc4731dd9361d8f55baaf02c9a6f66c6b
Brunão <brunobarretosantos@users.noreply.github.com>
2021-11-30 23:15:01 -0300

    tela de login

---
+++ containers/Login.tsx
@@ -0,0 +1,16 @@
+export const Login = () => {
+    return(
+        <div className="container-login">
+            <img src="logo.svg" alt="Logo Fiap" className="logo" />
+            <div className="input">
+                <img src="mail.svg"/>
+                <input />
+            </div>
+            <div className="input">
+                <img src="lock.svg"/>
+                <input />
+            </div>
+            <button>Login</button>
+        </div>
+    )
+}
