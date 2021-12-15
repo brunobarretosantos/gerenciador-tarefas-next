7018a0657adf66bc3e840175f68d775004322625
Brunão <brunobarretosantos@gmail.com>
2021-12-14 21:14:29 -0300

    remove unnecessary logs

--- middlewares/jwtValidator.ts
+++ middlewares/jwtValidator.ts
@@ -19,9 +19,6 @@ const jwtValidator = (handler: NextApiHandler) =>
             try {
                 const authorization = req.headers['authorization'];
 
-                console.log("authorization");
-                console.log(authorization);
-
                 if (!authorization){
                     return res.status(400).json({ error: 'Token is required' })
                 }
@@ -32,23 +29,15 @@ const jwtValidator = (handler: NextApiHandler) =>
                     return res.status(400).json({ error: 'Token is required' })
                 }
 
-                console.log("MY_SECRET_KEY");
-                console.log(MY_SECRET_KEY);
-
                 const decode = await jwt.verify(token, MY_SECRET_KEY) as JwtPayload;
-
-                console.log("decode");
-                console.log(decode);
 
                 if (!decode){
                     return res.status(400).json({ error: 'Invalid Token' })
                 }
 
                 if (req.body){
-                    console.log("body");
                     req.body.userId = decode._id;
                 }else{
-                    console.log("query");
                     req.query.userId = decode._id;
                 }
             } catch (error) {
